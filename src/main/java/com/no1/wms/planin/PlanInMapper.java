17149e1dafa08c80edad33037b44189ab5da4150
김민구 <mcutegs2@me.com>
2024-01-16 22:23:06 +0900

    planin 작업 중

--- src/main/java/com/no1/wms/planin/PlanInMapper.java
+++ src/main/java/com/no1/wms/planin/PlanInMapper.java
@@ -1,5 +1,18 @@
 package com.no1.wms.planin;
 
-public class PlanInMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.no1.wms.account.AccountDto;
+
+@Repository
+@Mapper
+public interface PlanInMapper {
+	
+    List<AccountDto> selectAll(Map<String, Object> m);
+	
+    int count(Map<String, Object> m);
 }
